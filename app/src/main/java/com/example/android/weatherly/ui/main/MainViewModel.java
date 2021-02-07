e02dbc6ddadb7569a8b523636f85e587045f60cf
YousefAbood <32194247+YousefAbood@users.noreply.github.com>
2021-02-07 23:41:22 +0100

    Revert "Revert "Replace SavedStateHandle with SharedPreferences""

--- app/src/main/java/com/example/android/weatherly/ui/main/MainViewModel.java
+++ app/src/main/java/com/example/android/weatherly/ui/main/MainViewModel.java
@@ -1,21 +1,37 @@
 package com.example.android.weatherly.ui.main;
 
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 
-public class MainViewModel extends ViewModel {
-    private final SavedStateHandle handle;
-    private final MutableLiveData<List<String>> cityNames;
+public class MainViewModel extends AndroidViewModel {
+    private final MutableLiveData<List<String>> cityNames = new MutableLiveData<>(Collections.emptyList());
 
-    public MainViewModel(SavedStateHandle handle) {
-        this.handle = handle;
-        this.cityNames = handle.getLiveData("cityNames", Collections.emptyList());
+    private final SharedPreferences sharedPreferences;
+    private final Gson gson = new Gson();
+
+    @SuppressWarnings("deprecation")
+    public MainViewModel(Application application) {
+        super(application);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(application);
+
+        String jsonNames = sharedPreferences.getString("cityNames", "[]");
+
+        cityNames.setValue(gson.fromJson(jsonNames, new TypeToken<ArrayList<String>>() {}.getType()));
     }
 
     public LiveData<List<String>> getCityNames() {
@@ -30,5 +46,7 @@ public class MainViewModel extends ViewModel {
         }
 
         cityNames.setValue(Collections.unmodifiableList(newCityNames));
+
+        sharedPreferences.edit().putString("cityNames", gson.toJson(newCityNames)).apply();
     }
 }
